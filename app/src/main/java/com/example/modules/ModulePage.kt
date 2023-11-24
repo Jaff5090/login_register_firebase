c8197e5810487307b2548d56552ef692a97ed0fb
Jaff5090 <94871723+Jaff5090@users.noreply.github.com>
2023-11-24 17:35:31 +0100

    add module pages

---
+++ app/src/main/java/com/example/modules/ModulePage.kt
@@ -0,0 +1,12 @@
+package com.example.modules
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.login_register_firebase.R
+
+class ModulePage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_module_page)
+    }
+}
